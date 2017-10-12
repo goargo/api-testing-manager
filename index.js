597a7de7c292778816630eac822b3cf356ed1b17
Hossam Saraya <hossam.saraya@gmail.com>
2017-10-12 15:38:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 require('shelljs/global');
+const Promise = require('bluebird');
 const express = require('express');
 const app = express();
 var isRunning = false;
+var activeBranch;
 
+cd(__dirname + '/../api');
+
+// Sync
 var buildBranch = function (branch) {
+  if (isRunning) {
+    return false;
+  }
   isRunning = true;
-  cd(__dirname + '/../api');
   exec('echo "FLUSHALL" | redis-cli');
   exec(
-    "pg_dump --dbname=postgresql:/__DBURL__/ebdb --format t -f staging_dump.sql");
+    "pg_dump --dbname=postgresql://argoapi:__DBURL__:5432/ebdb --format t -f staging_dump.sql"
+  );
   exec('pg_restore --clean -d argo_api_testing staging_dump.sql');
   exec('rm -rf staging_dump.sql');
-  exec('killall ruby && killall bundle');
-  exec('rails s -d -p 80 &');
+  exec('sudo killall ruby && sudo killall bundle');
+  exec('bundle install');
+  exec('rake db:migrate');
+  exec('rbenv sudo rails s -d -p 80 &');
   exec('bundle exec sidekiq -d -l sidekiq.log &');
   isRunning = false;
+  activeBranch = branch;
 };
 
-app.get(/^\/(.*)/, function (req, res) {
+app.get('/checkout', function (req, res) {
   if (isRunning) {
     res.send('Already Building');
   } else {
-    var branch = req.params[0];
+    var branch = req.query.branch;
+    exec('git fetch origin');
+    if (exec('git checkout origin/' + branch)
+      .code !== 0)
+      return res.send('branch ' + branch + ' doesnt exist');
+    res.send('Building branch ' + branch +
+      '. Please be patient, it can take up to a minute to deploy the branch. It will be active at http://testing.api.goargo.com'
+    );
+    res.end();
     buildBranch(branch);
-    res.send('done');
   }
-  res.end();
+});
+
+app.get('/status', function (req, res) {
+  res.send(activeBranch);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('listening on port 3000!')
 })
